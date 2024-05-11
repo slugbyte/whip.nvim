1214d5c7f7121e2de112d88ec1d7bff2ef6f9ae8
Duncan Marsh <slugbyte@slugbyte.com>
2024-05-11 22:26:03 +0800

    scratchpad keeps track of last selected file

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # scratchpad.nvim
 > quickly edit and manage scratchpads
 
-## WARNING: this is under construction (don't use it)
-
 ## SETUP
 ```lua
 -- lazy nvim setup
@@ -13,21 +11,43 @@ return {
         "nvim-telescope/telescope.nvim",
     },
     config = function()
-        require("scratchpad").setup({
-            root_dir = "<put you scrathpad directory here>"
+        local scratchpad = require("scratchpad")
+        scratchpad.setup({
+            dir = "~/your/scratchpad/dir/goes-here"
         })
+        vim.keymap.set("", "<leader>so", scratchpad.open, { desc = "[S]cratchpad [O]pen" })
+        vim.keymap.set("", "<leader>sm", scratchpad.make, { desc = "[S]cratchpad [M]ake" })
+        vim.keymap.set("", "<leader>sf", scratchpad.find_file, { desc = "[S]cratchpad [F]ile Search" })
+        vim.keymap.set("", "<leader>sg", scratchpad.find_grep, { desc = "[S]cratchpad [G]rep Search" })
     end,
 }
-
 ```
 
 ## USAGE
-* `:Sfind` - telescope find files of scratchpad dir
-* `:Sgrep` - telescope find files of scratchpad dir
-* `:Snew <filename>` - add a new file in the scratchpad dir
+scratchpad.nvim allows you to quickly edit files in the scratchpad `dir`
+configured with `setup()`. The main benefit it provides over vanilla telescope.nvim is that
+it remembers the last scratchpad you edited.
 
-### TODO
-* Save last selected file and allways open to it
-* ?? git commit push on write
-* Create a ui for scratchpad crud
-* Allow custom keymaps
+## lua api
+* `scratchpad.open()` open last selected scratchpad file, if no file is selected
+it will trigger `scratchpad.find_file()`
+* `scratchpad.make()` prompt to create a new scratchpad file
+* `scratchpad.find_file()` telescope scratchpad files
+* `scratchpad.find_grep()` telescope grep scratchpad files
+
+## commands
+* `:ScratchpadOpen` calls `scratchpad.open()`
+* `:ScratchpadMake` calls `scratchpad.make()`
+* `:ScratchpadFindFile` calls `scratchpad.find_file()`
+* `:ScratchpadFindGrep` calls `scratchpad.find_grep()`
+
+I use different scratchpads for different projects, that I tend to reference a lot while I'm working.
+I find it super nice to be able to quickly pop open then scratchpad for what
+ever I'm working on without having to find_files every time.
+
+## NOTE
+scratchpad.nvim creates a `.scratchpad.json` in your scratchpad `dir` to keep track of
+your last selected file.
+
+## TODO:
+* health check
--- lua/scratchpad/init.lua
+++ lua/scratchpad/init.lua
@@ -1,42 +1,112 @@
-local telescope_status, _ = pcall(require, "telescope")
-
-local M = {}
-local state = {
-    root_dir = nil,
-}
-
-local _, t_builtin = pcall(require, "telescope.builtin")
-local _, t_pickers = pcall(require, "telescope.pickers")
-local _, t_finders = pcall(require, "telescope.finders")
+local plenary_found, Path = pcall(require, "plenary.path")
+local telescope_found, t_builtin = pcall(require, "telescope.builtin")
 local _, t_actions = pcall(require, "telescope.actions")
 local _, t_action_state = pcall(require, "telescope.actions.state")
 local _, t_action_set = pcall(require, "telescope.actions.set")
 
+local M = {}
+local state = {
+    dir = nil,
+    config_path = nil,
+    config_data = {
+        current = nil,
+    },
+}
+
+
 local log_error = function(fmt, ...)
-    vim.notify(string.format(fmt, ...), vim.log.levels.ERROR)
+    vim.notify("ERROR: scratchpad.nvim " .. string.format(fmt, ...), vim.log.levels.ERROR)
 end
 
-local log_info = function(fmt, ...)
-    vim.notify(string.format(fmt, ...), vim.log.levels.INFO)
-end
-
-local telescope_find_files = function()
-    if not telescope_status then
-        return log_error("telescope not found")
+local config_check_exists = function()
+    if state.config_path == nil then
+        return false
     end
-    if state.root_dir == nil then
-        return log_error("error no scratchpad root_dir")
+    return Path:new(state.config_path):exists()
+end
+
+local current_scratchpad_path = function()
+    if state.config_data == nil or state.config_data.current == nil then
+        return nil
+    end
+    return string.format("%s/%s", state.dir, state.config_data.current)
+end
+
+local current_scratchpad_check_exists = function()
+    local path = current_scratchpad_path()
+    if path == nil then
+        return false
+    end
+    return Path:new(current_scratchpad_path()):exists()
+end
+
+local config_load = function()
+    if not config_check_exists() then
+        return
+    end
+    if state.config_path == nil then
+        return
+    end
+    local read_ok, config_json = pcall(function()
+        return Path:new(state.config_path):read()
+    end)
+    if not read_ok then
+        return
+    end
+    local decode_ok, config_data = pcall(vim.fn.json_decode, config_json)
+    if not decode_ok then
+        return log_error("cannot parse .scratchpad.json")
+    end
+    state.config_data = config_data
+end
+
+local config_save = function()
+    if state.config_path == nil then
+        return
+    end
+
+    local write_ok, _ = pcall(function()
+        Path:new(state.config_path):write(vim.fn.json_encode(state.config_data), "w")
+    end)
+
+    if not write_ok then
+        return log_error("faled to update config: could not wite to %s", state.config_path)
+    end
+end
+
+local state_set_dir = function(path)
+    local dir_path = Path:new(path)
+    if not dir_path:exists() then
+        log_error("opts.dir does not exist: %", path)
+        return
+    end
+    if not dir_path:is_dir() then
+        log_error("opts.dir is not a directory: %", path)
+        return
+    end
+    state.dir = dir_path:expand()
+    state.config_path = string.format("%s/.scratchpad.json", state.dir)
+end
+
+M.find_file = function()
+    if not telescope_found then
+        return log_error("could not find telescope")
+    end
+    if state.dir == nil then
+        return log_error("error no scratchpad dir")
     end
     t_builtin.find_files({
-        hidden = true,
-        cwd = state.root_dir,
+        cwd = state.dir,
+        prompt_title = "Find Scratchpad",
         attach_mappings = function(prompt_bufnr, _)
             t_actions.select_default:replace(function()
                 -- save selected state
                 local selection = t_action_state.get_selected_entry()
-                vim.defer_fn(function()
-                    log_error("selected: %s", selection[1])
-                end, 0)
+                if selection[1] ~= "" then
+                    state.config_data.current = selection[1]
+                    config_save()
+                end
+
                 -- do original select
                 t_action_set.select(prompt_bufnr, "default")
             end)
@@ -45,48 +115,65 @@ local telescope_find_files = function()
     })
 end
 
-local telescope_live_grep = function()
-    if not telescope_status then
-        return log_error("telescope not found")
+M.find_grep = function()
+    if not telescope_found then
+        return log_error("could not find telescope.nvim")
     end
-    if state.root_dir == nil then
-        return log_error("error no scratchpad root_dir")
+    if state.dir == nil then
+        return log_error("error no scratchpad dir")
     end
     t_builtin.live_grep({
-        cwd = state.root_dir,
+        cwd = state.dir,
         additional_args = { "--hidden" },
-
+        attach_mappings = function(prompt_bufnr, _)
+            t_actions.select_default:replace(function()
+                -- save selected state
+                local selection = t_action_state.get_selected_entry()
+                if selection.filename ~= "" then
+                    state.config_data.current = selection.filename
+                    config_save()
+                end
+                -- do original select
+                t_action_set.select(prompt_bufnr, "default")
+            end)
+            return true
+        end
     })
 end
 
-M.setup = function(config)
-    if config == nil then
-        config = {}
+M.make = function()
+    local input = vim.fn.input({
+        prompt = "create scratchpad: "
+    })
+    vim.cmd(string.format("edit %s/%s", state.dir, input))
+end
+
+M.open = function()
+    if not current_scratchpad_check_exists() then
+        return M.find_file()
+    end
+    vim.cmd(string.format("edit %s", current_scratchpad_path()))
+end
+
+M.setup = function(opts)
+    opts = opts or {}
+    if not telescope_found then
+        return log_error("could not find telescope.nvim")
+    end
+    if not plenary_found then
+        return log_error("could not find plenary.nvim")
+    end
+    if opts.dir then
+        state_set_dir(opts.dir)
+        if state.config_path ~= nil then
+            config_load()
+        end
     end
 
-    if not telescope_status then
-        return log_error("scratchpad.nvim error: telescope not found")
-    end
-
-    if config.root_dir then
-        state.root_dir = config.root_dir
-    end
-
-    vim.api.nvim_create_user_command("Sfind", telescope_find_files, {})
-    vim.api.nvim_create_user_command("Sgrep", telescope_live_grep, {})
-    vim.api.nvim_create_user_command("Snew",
-        function(opt)
-            -- TODO: make this a prompt instead
-            local name = opt.args
-            vim.cmd(string.format(":e %s/%s", state.root_dir, name))
-        end,
-        { nargs = 1 }
-    )
-
-    -- vim.keymap.set("", "<leader><leader>w", , { desc = "test" })
-    -- test_picker({})
-    vim.keymap.set("", "<leader>w", telescope_find_files, { desc = "Scratchpad File Search" })
-    vim.keymap.set("", "<leader>W", telescope_find_files, { desc = "Scratchpad Grep Search" })
+    vim.api.nvim_create_user_command("ScratchpadOpen", M.open, {})
+    vim.api.nvim_create_user_command("ScratchpadMake", M.make, {})
+    vim.api.nvim_create_user_command("ScratchpadFindFile", M.find_file, {})
+    vim.api.nvim_create_user_command("ScratchpadFindGrep", M.find_grep, {})
 end
 
 return M
